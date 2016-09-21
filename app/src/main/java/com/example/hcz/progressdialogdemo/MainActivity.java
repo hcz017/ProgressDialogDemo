06f1c2f08720a14a0e3d62afa1adf6334dadce83
chengzhi.hou@ck-telecom.com <chengzhi.hou@ck-telecom.com>
2016-09-21 14:46:33 +0800

    use java reflect to access hide methods

--- app/src/main/java/com/example/hcz/progressdialogdemo/MainActivity.java
+++ app/src/main/java/com/example/hcz/progressdialogdemo/MainActivity.java
@@ -11,15 +11,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class MainActivity extends AppCompatActivity {
 
     boolean progressComplete = true;
     ProgressDialog sProgressDialog;
+    TextView tvOpName;
     private static final String TAG = "MainActivity";
     private static final int TASK_COMPLETE = 0;
     private static final int NOT_TASK_COMPLETE = 1;
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        tvOpName = (TextView) findViewById(R.id.operator_name);
         Button btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -164,5 +172,24 @@ public class MainActivity extends AppCompatActivity {
     public void TestNotificationExtra(View view){
         Intent intent = new Intent(this, MyNotification.class);
         startActivity(intent);
+    }
+
+    //java reflect to access hide methods.
+    public void getNetworkOperatorName(View view) throws ClassNotFoundException,
+            NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // get defaultDataSubId  @hide
+        Class subscriptionManager;
+        subscriptionManager = Class.forName("android.telephony.SubscriptionManager");
+        Method getDefaultDataSubId = subscriptionManager.getMethod("getDefaultDataSubId");
+        Object defaultDataSubId = getDefaultDataSubId.invoke(null);
+        // to get getNetworkOperatorName(subId), @hide
+        TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        Class telephonyManager;
+        telephonyManager = Class.forName("android.telephony.TelephonyManager");
+        Method getNetworkOperatorName = telephonyManager.getMethod("getNetworkOperatorName", int.class);
+        getNetworkOperatorName.setAccessible(true);
+        Object operatorName = getNetworkOperatorName.invoke(tm, defaultDataSubId);
+        tvOpName.setText((String) operatorName);
+        Log.i(TAG, "default data sub network operator name : " + operatorName);
     }
 }
